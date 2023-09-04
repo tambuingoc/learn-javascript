8518868559476c83448ebdf6a1ae6a37b6b5a788
tambuingoc <dangtuan25051975@gmail.com>
2023-09-04 16:10:07 +0700

    second commit

---
+++ js-basic/04-function/README.md
@@ -0,0 +1,6 @@
+Function
+1. Declaration function: được định nghĩa bằng từ khóa function + bắt buộc phải đặt tên
+- Cho phép gọi hàm trước khi định nghĩa
+2. Expression Function
+3. Arrow Function
+
---
+++ js-basic/04-function/main.js
@@ -0,0 +1,19 @@
+//Declaration function
+function deClarationFunction() {
+    console.log('Declaration Function');
+}
+
+//Expression Function
+var expressionFunction = function() {
+    console.log('Expression Function');
+}
+
+var myObj = {
+    myFunction: function() {
+
+    }
+}
+
+setTimeout(function() {});
+
+//Arrow function
